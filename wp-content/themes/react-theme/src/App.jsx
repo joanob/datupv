5051237c33325959c225924f5a16328167d4f23e
joanob <54626555+joanob@users.noreply.github.com>
2022-11-11 18:50:56 +0100

    use wp rest api

--- wp-content/themes/react-theme/src/App.jsx
+++ wp-content/themes/react-theme/src/App.jsx
@@ -1,31 +1,19 @@
-import { useState } from 'react'
-import './App.css'
+import { useState, useEffect } from "react";
+
+const wp = "http://localhost/dat/wp-json/wp/v2"
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [posts, setPosts] = useState([])
+
+  useEffect(() => {
+    fetch(wp + "/posts").then(res => res.json()).then(res => {setPosts(res)})
+  }, [])
+  
+  console.log(posts)
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={""} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      
     </div>
   )
 }
