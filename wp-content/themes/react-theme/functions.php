b9e72d1a752f5fd05f17d6131c5a276c9ab18fdf
joanob <54626555+joanob@users.noreply.github.com>
2022-11-11 16:55:53 +0100

    fix react functions.php

--- wp-content/themes/react-theme/functions.php
+++ wp-content/themes/react-theme/functions.php
@@ -1,14 +1,14 @@
+<?php
 function react_enqueue_scripts() {
 
     wp_enqueue_script("theme", get_stylesheet_directory_uri() . "/dist/assets/index.js", array(), "1.0", true);
     wp_enqueue_style("theme", get_stylesheet_directory_uri() . "/dist/assets/index.css", array(), "1.0");
 
-    <!-- 
-    $config = array(
+    /* $config = array(
         // Add any theme variables needed in react
     );
-    wp_localize_script('theme', 'wp_config', $config); 
-    -->
+    wp_localize_script('theme', 'wp_config', $config);  */
+    
 }
 
 add_action("wp_enqueue_scripts", "react_enqueue_scripts");
