2833462eaa8085e0b5a247bb7ff219fc68aec993
joanob <54626555+joanob@users.noreply.github.com>
2022-11-18 19:02:15 +0100

    deploy

--- wp-content/themes/design/src/App.tsx
+++ wp-content/themes/design/src/App.tsx
@@ -1,20 +1,17 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import { nav } from "./data/nav";
 import Header from "./layout/Header";
 import Home from "./pages/Home";
 
 const App = () => {
-  /* gh-pages basename: /datupv/ */
-  const baseUrl = import.meta.env.DEV ? "/datupv" : "/";
-
   return (
-    <BrowserRouter basename={"/datupv" + baseUrl}>
+    <HashRouter>
       <Header nav={nav} />
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
       <footer className="footer"></footer>
-    </BrowserRouter>
+    </HashRouter>
   );
 };
 
