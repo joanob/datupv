f2b22c0f5a0b55ba016dfb9e146ccae5071661e6
joanob <54626555+joanob@users.noreply.github.com>
2022-11-18 18:43:56 +0100

    deploy

--- wp-content/themes/design/src/App.tsx
+++ wp-content/themes/design/src/App.tsx
@@ -4,8 +4,11 @@ import Header from "./layout/Header";
 import Home from "./pages/Home";
 
 const App = () => {
+  /* gh-pages basename: /datupv/ */
+  const baseUrl = "/datupv/";
+
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={baseUrl}>
       <Header nav={nav} />
       <Routes>
         <Route path="/" element={<Home />} />
