326315b6e3d0f83caab928ac96ee4b999ba6b01c
joanob <54626555+joanob@users.noreply.github.com>
2022-11-21 18:14:04 +0100

    add desktop swiper support

--- wp-content/themes/design/src/pages/Home/index.tsx
+++ wp-content/themes/design/src/pages/Home/index.tsx
@@ -9,11 +9,21 @@ import { feed } from "../../data/feed";
 const NewsCarousel = () => {
   const news = feed.length > 5 ? feed.slice(0, 5) : feed;
 
+  const maxWidth = news.length * 320 + (news.length - 1) * 25;
+
+  const slidesPerView =
+    document.body.offsetWidth < 500
+      ? 1
+      : document.body.offsetWidth > maxWidth
+      ? news.length
+      : "auto";
+
   return (
     <Swiper
-      slidesPerView={1}
+      slidesPerView={slidesPerView}
       spaceBetween={25}
-      loop={true}
+      centeredSlides={document.body.offsetWidth >= 500}
+      loop={slidesPerView === 1}
       pagination={{
         clickable: true,
       }}
@@ -23,6 +33,7 @@ const NewsCarousel = () => {
       }}
       navigation={true}
       modules={[Pagination, Navigation, Autoplay]}
+      style={{ maxWidth: maxWidth }}
     >
       {news.map((article) => (
         <SwiperSlide key={article.title}>
