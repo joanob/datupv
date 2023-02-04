f4d2934a6e9f902ac7901e9451a44842294c9509
joanob <54626555+joanob@users.noreply.github.com>
2023-02-04 14:12:19 +0100

    add link parser

--- client/src/components/Posts/TextSection/index.tsx
+++ client/src/components/Posts/TextSection/index.tsx
@@ -1,9 +1,34 @@
+import { Link } from "react-router-dom";
+import { linkParser } from "../../../helpers/linkParser";
+
+import "./textsection.css";
+
 interface Props {
   text: string;
 }
 
 const TextSection = ({ text }: Props) => {
-  return <p>{text}</p>;
+  return (
+    <p>
+      {linkParser(text).map((section, i) => {
+        if (section.texto !== undefined) {
+          if (section.link !== undefined) {
+            return (
+              <Link key={i} to={section.link}>
+                {section.texto}
+              </Link>
+            );
+          }
+          return (
+            <a key={i} href={section.url}>
+              {section.texto}
+            </a>
+          );
+        }
+        return <span key={i}>{section}</span>;
+      })}
+    </p>
+  );
 };
 
 export default TextSection;
---
+++ client/src/helpers/linkParser.ts
@@ -0,0 +1,51 @@
+/**
+ * Internal link: {link: string, text: string}
+ * External link: {url: string, text: string}
+ */
+export const linkParser = (text: string) => {
+  const parts = [];
+
+  let start = text.indexOf("{");
+  let end, part;
+  parts.push(text.substring(0, start));
+
+  while (start !== -1) {
+    end = text.indexOf("}", start);
+    part = text.substring(start, end + 1);
+
+    try {
+      const linkObject = JSON.parse(part);
+      if (linkObject.link !== undefined && linkObject.texto !== undefined) {
+        const link = linkObject.link.replace(
+          "http://" + import.meta.env.VITE_CLIENT_URL,
+          ""
+        );
+        parts.push({ link, texto: linkObject.texto });
+      } else if (
+        linkObject.url !== undefined &&
+        linkObject.texto !== undefined
+      ) {
+        parts.push(linkObject);
+      } else {
+        parts.push(JSON.stringify(linkObject));
+      }
+    } catch (err) {
+      parts.push(part);
+    }
+
+    start = text.indexOf("{", end);
+    if (start === -1) {
+      parts.push(text.substring(end + 1, text.length));
+      break;
+    }
+    if (start !== end) {
+      parts.push(text.substring(end + 1, start));
+    }
+  }
+
+  if (end === undefined) {
+    parts.push(text);
+  }
+
+  return parts;
+};
