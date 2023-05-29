fe59b7a94f2c802ef0d9a2c18ef347e7a2a3b8ce
joanob <54626555+joanob@users.noreply.github.com>
2023-05-29 11:38:04 +0200

    add mysql support

--- server/config/database.js
+++ server/config/database.js
@@ -1,11 +1,57 @@
 const path = require('path');
+require("mysql")
 
-module.exports = ({ env }) => ({
-  connection: {
-    client: 'sqlite',
-    connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+// https://docs.strapi.io/dev-docs/configurations/database#environment-variables-in-database-configurations
+
+module.exports = ({ env }) => {
+  const client = env("DATABASE_CLIENT")
+
+  const connections = {
+    sqlite: {
+      connection: {
+        filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      },
+      useNullAsDefault: true,
     },
-    useNullAsDefault: true,
-  },
-});
+    mysql: {
+      connection: {
+        client: "mysql",
+        connection: {
+          host: env('DATABASE_HOST'),
+          port: env.int('DATABASE_PORT'),
+          database: env('DATABASE_NAME'),
+          user: env('DATABASE_USERNAME'),
+          password: env('DATABASE_PASSWORD'),
+          timezone: "Europe/Berlin",
+          ssl: false
+        }
+      }
+    }
+  }
+
+  return {
+    client,
+    ...connections[client],
+    debug: true,
+    acquireConnectionTimeout: env.int('DATABASE_CONNECTION_TIMEOUT', 30000)
+  }
+
+
+
+  /* defaultConnection: env("DATABASE_CONNECTION_NAME"),
+  connections: {
+    sqlite: {
+      client: 'sqlite',
+      connection: {
+        filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      },
+      useNullAsDefault: true,
+    },
+    mysql: {
+      client: "mysql",
+      settings: {
+ 
+      }
+    }
+  } */
+};
