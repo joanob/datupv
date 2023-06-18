d0d0da5b00f12a7a786c99f4e5edc0fa0b068c79
joanob <54626555+joanob@users.noreply.github.com>
2023-06-18 16:30:28 +0200

    fix merge issues

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -5,14 +5,12 @@ import Router from "./Router";
 import { Toaster } from "react-hot-toast";
 
 import "./styles/styles.scss";
-import Home from "./pages/Home";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <NavContextWrapper>
       <Toaster position="bottom-center" toastOptions={{ duration: 3000 }} />
-      {/* <Router /> */}
-      <Home />
+      <Router />
     </NavContextWrapper>
   </React.StrictMode>
 );
